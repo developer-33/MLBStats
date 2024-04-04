a95edb887c23e3e51be4e5a1c2cd8950889803f3
fletcher burton <burton.fletcher@aol.com>
2024-04-04 10:40:31 -0500

    first commit

---
+++ yankee.js
@@ -0,0 +1,335 @@
+const yankeeHitters = {
+
+    "Oswaldo Cabrera": {
+    PlayerName:"Oswaldo Cabrera",   
+    postion: "3B",
+    games: 7, 
+    atBat: 26,
+    runs: 4,
+    hits: 9,
+    doubles: 1,
+    triples: 0,
+    homeRuns: 2,
+    rBI: 7,
+    walksBB: 2,
+    strikeouts: 7,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .346,
+    onBasePercentage: .393,
+    sluggingPercentage: .615,
+    onBasePlusSluggingPercentage: 1.008
+},   
+
+   "Juan Soto": {
+    PlayerName:"Juan Soto",   
+    postion: "RF",
+    games: 7,
+    atBat: 29,
+    runs: 3,
+    hits: 10,
+    doubles: 1,
+    triples: 0,
+    homeRuns: 1,
+    rBI: 4,
+    walksBB: 5,
+    strikeouts: 4,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .345,
+    onBasePercentage: .441,
+    sluggingPercentage: .483,
+    onBasePlusSluggingPercentage: .924
+},
+  "Giancarlo Stanton": {
+    PlayerName:"Giancar Stanton",   
+    postion: "DH",
+    games: 5,
+    atBat: 20,
+    runs: 2,
+    hits: 3,
+    doubles: 1,
+    triples: 0,
+    homeRuns: 1,
+    rBI: 1,
+    walksBB: 1,
+    strikeouts: 11,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .150,
+    onBasePercentage: .190,
+    sluggingPercentage: .350,
+    onBasePlusSluggingPercentage: .540
+
+},
+    "Anthony Volpe": {
+    PlayerName:"Anthony Volpe",   
+    postion: "ss",
+    games: 5,
+    atBat: 17,
+    runs: 5,
+    hits: 9,
+    doubles: 3,
+    triples: 0,
+    homeRuns: 1,
+    rBI: 3,
+    walksBB: 4,
+    strikeouts: 5,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage:.529,
+    onBasePercentage: .619,
+    sluggingPercentage: 882,
+    onBasePlusSluggingPercentage: 1.501
+
+},
+
+    // 5
+    "Jon Berti": {
+    PlayerName:"Jon Berti",   
+    postion: "3B",
+    games: 1,
+    atBat: 4,
+    runs: 0,
+    hits: 1,
+    doubles: 0,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 1,
+    walksBB: 0,
+    strikeouts: 2,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .250,
+    onBasePercentage: .250,
+    sluggingPercentage: .250,
+    onBasePlusSluggingPercentage: .500
+
+},
+
+
+    "Trent Grisham": {
+     PlayerName:"Trent Grisham",   
+    postion: "CF",
+    games: 1,
+    atBat: 3,
+    runs: 0,
+    hits: 0,
+    doubles: 0,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 0,
+    walksBB: 1,
+    strikeouts: 2,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .000,
+    onBasePercentage: .250,
+    sluggingPercentage: .000,
+    onBasePlusSluggingPercentage: .250
+
+},
+
+    "Jahmai Jones": {
+    PlayerName:"Jahma Jones" ,
+    postion: "2B",
+    games: 1,
+    atBat: 1,
+    runs: 0,
+    hits: 0,
+    doubles: 0,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 0,
+    walksBB: 0,
+    strikeouts: 1,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .000,
+    onBasePercentage: .000,
+    sluggingPercentage: .000,
+    onBasePlusSluggingPercentage: .000,
+
+},
+
+    "Aaron Judge": {
+    PlayerName:"Aaron Judge",    
+    postion: "CF",
+    games: 6,
+    atBat: 24,
+    runs: 1,
+    hits: 3,
+    doubles: 1,
+    triples: 0,
+    homeRuns: 1,
+    rBI: 1,
+    walksBB: 3,
+    strikeouts: 7,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .125,
+    onBasePercentage: .214,
+    sluggingPercentage: .167,
+    onBasePlusSluggingPercentage: .381
+
+},
+
+
+    "Anothony Rizzo": {
+        PlayerName:"Anothony Rizzo",    
+    postion: "1B",  
+    games: 6,
+    atBat: 22,
+    runs: 3,
+    hits: 4,
+    doubles: 1,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 1,
+    walksBB: 3,
+    strikeouts: 5,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .182,
+    onBasePercentage: .308,
+    sluggingPercentage: .227,
+    onBasePlusSluggingPercentage: .535
+
+},
+
+
+    // 10 
+    "Gleyber Torres":  {
+        PlayerName:"Gleyber Torres",    
+    postion: "2B",
+    games: 6,
+    atBat: 23,
+    runs: 3,
+    hits: 4,
+    doubles: 1,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 1,
+    walksBB: 4,
+    strikeouts: 5,
+    stolenBases : 1,
+    caughtStealing: 0,
+    battingAverage: .174,
+    onBasePercentage: .321,
+    sluggingPercentage: .217,
+    onBasePlusSluggingPercentage: .538
+
+},
+
+
+    // 11
+    "Jose Trevino": {
+    PlayerName:"Jose Trevino",       
+    postion: "C",
+    games: 3,
+    atBat: 9,
+    runs: 1,
+    hits: 1,
+    doubles: 0,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 1,
+    walksBB: 1,
+    strikeouts: 3,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .111,
+    onBasePercentage: .200,
+    sluggingPercentage: .111,
+    onBasePlusSluggingPercentage: .311
+
+},
+
+
+    // 12
+    "Alex Verdugo": {
+    PlayerName:"Alex Verdugo",   
+    postion: "LF",
+    games: 6,
+    atBat: 21,
+    runs: 2,
+    hits: 3,
+    doubles: 0,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 1,
+    walksBB: 2,
+    strikeouts: 2,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .143,
+    onBasePercentage: .208,
+    sluggingPercentage: .143,
+    onBasePlusSluggingPercentage: .351,
+
+},
+
+
+    // 13
+    "Austin Wells": {
+    PlayerName:"Austin Wells",   
+    postion: "c",
+    games: 4,
+    atBat: 10,
+    runs: 3,
+    hits: 2,
+    doubles: 0,
+    triples: 0,
+    homeRuns: 0,
+    rBI: 1,
+    walksBB: 3,
+    strikeouts: 1,
+    stolenBases : 0,
+    caughtStealing: 0,
+    battingAverage: .200,
+    onBasePercentage: .375,
+    sluggingPercentage: .200,
+    onBasePlusSluggingPercentage: .557
+
+}
+};
+
+// console.log(yankeeHitters["Austin Wells"[4]]);
+// // console.log(yankeeHitters["Juan Soto"]);
+   
+
+// const juanSotoBattingAverage = yankeeHitters["Juan Soto"].battingAverage;
+// console.log(juanSotoBattingAverage);
+
+// function updatePlayerData(PlayerName, newData) {
+//     if (yankeeHitters[PlayerName]) {
+//       Object.assign(yankeeHitters[PlayerName], newData);
+//     } else {
+//       console.log(`Player ${PlayerName} not found.`);
+//     }
+//   }
+ 
+//   // Example usage:
+//   updatePlayerData("Juan Soto", { games: 8, hits: 12, homeRuns: 2 });
+
+// console.log(updatePlayerData)
+
+
+let updatePlayerData = (PlayerName, newData) => {
+    if(yankeeHitters[PlayerName]){
+    Object.assign(yankeeHitters[PlayerName], newData)
+    }else{
+        console.log(`player ${PlayerName} not found.`)
+    }
+}
+
+updatePlayerData(
+    "Juan Soto", { games: 8, hits: 12, homeRuns: 88 },
+)
+updatePlayerData(
+  "Aaron Judge",{ games: 34, hits: 45, homeRuns: 2, atBat:36}
+)
+   
+    console.log(yankeeHitters)
+  
+
